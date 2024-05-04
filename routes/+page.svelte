c8bd376e796333ba26520d0a060d1927dc519a46
r0mashcka <145837752+r0mashckaa@users.noreply.github.com>
2024-05-04 22:53:59 +0500

    Add files via upload

---
+++ routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import MainBody from '../components/MainBody.svelte';
+</script>
+
+<div class="all">
+	<MainBody />
+</div>
+
+<style>
+    .all {
+        cursor: url('sword.png'), auto;
+        display: flex;
+        background: url(bg.png) no-repeat center;
+        background-size: 100%;
+        height: 1764px;
+        background-position: 0 0;
+    }
+</style>
---
+++ routes/heroes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import Heroes from "../../components/heroes.svelte";
+
+</script>
+<div class="all">
+	<Heroes />
+</div>
+
+<style>
+	.all {
+		cursor: url('sword.png'), auto;
+		display: flex;
+		background: url(bg2.png) no-repeat center;
+		background-size: 100%;
+        height: 3996px;
+		background-position: 0 0;
+	}
+</style>
